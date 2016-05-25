bb9c69866c8aa223f5115926b241cc46c1b2346c
treba <treba@localhost.localdomain>
2016-05-25 11:39:25 +0200

    #376 and #378

--- src/org/loklak/tools/storage/JsonFile.java
+++ src/org/loklak/tools/storage/JsonFile.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.Key;
 import java.security.KeyFactory;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -138,8 +139,16 @@ public class JsonFile extends JSONObject {
 		return private_key;
 	}
 	
+	public String getPrivateKeyAsString(){
+		return getKeyAsString(private_key);
+	}
+	
 	public PublicKey getPublicKey(){
 		return public_key;
+	}
+	
+	public String getPublicKeyAsString(){
+		return getKeyAsString(public_key);
 	}
 	
 	public String getKeyMethod(){
@@ -148,6 +157,10 @@ public class JsonFile extends JSONObject {
 	
 	public String getPeerHash(){
 		return peer_hash;
+	}
+	
+	public String getKeyAsString(Key key){
+		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
 	
 	public void setPeerHash(){
@@ -198,7 +211,7 @@ public class JsonFile extends JSONObject {
 	}
 	
 	public boolean setPrivateKey(PrivateKey key, String algorithm){
-		put("private_key", Base64.getEncoder().encodeToString(key.getEncoded()));
+		put("private_key", getKeyAsString(key));
 		private_key = key;
 		put("key_method",algorithm);
 		key_method = algorithm;
@@ -206,7 +219,7 @@ public class JsonFile extends JSONObject {
 	}
 	
 	public boolean setPublicKey(PublicKey key, String algorithm){
-		put("public_key", Base64.getEncoder().encodeToString(key.getEncoded()));
+		put("public_key", getKeyAsString(key));
 		public_key = key;
 		put("key_method",algorithm);
 		key_method = algorithm;
