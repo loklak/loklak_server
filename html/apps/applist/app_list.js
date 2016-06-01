b5693c15f9d173f7b496d6ed21d301062f54c7e3
daminisatya <daminisatya@gmail.com>
2016-06-02 00:47:35 +0530

    updated the apps page

--- html/apps/applist/app_list.js
+++ html/apps/applist/app_list.js
@@ -7,16 +7,19 @@ app.controller("app_list", function($scope, $http) {
     .success(function(data, status, headers, config) {
         $scope.categoryKeys = data.categories;
         $scope.apps = data.apps;
+        $scope.categoryKeys.unshift('All');
     });
-    console.log($scope.apps);
 
     $scope.categoryFilter = function(event) {
-        console.log(event.target.id);
         item = event.target.id;
-        item = item.replace(/ /g, '');
-        $('div.span2').hide();
-        qConstruct = '#'+item;
-        $(qConstruct).show();
+        if (item != 'All') {
+            $('div.span2').hide();
+            qConstruct = 'div.span2#'+item;
+            $(qConstruct).show();
+        }
+        else {
+            $('div.span2').show();
+        }
     }
 });
 
@@ -24,56 +27,4 @@ app.filter('nospace', function () {
     return function (value) {
         return (!value) ? '' : value.replace(/ /g, '');
     };
-});
-
-angular.module("loklak")
-.config(function ($routeProvider) {
-    $routeProvider.
-    when("#AboutProject", { 
-        controller: "init('About project')",
-        activetab: "AboutProject"
-    }).
-    when("#Demo", {
-        controller: "init('Demo')",
-        activetab: "Demo"
-    }).
-    when("#MessageSearch", {
-        controller: "init('Message Search')",
-        activetab: "MessageSearch"
-    }).
-    when("#TweetsAnalytics", {
-        controller: "init('Tweets analytics')",
-        activetab: "TweetsAnalytics"
-
-    }).
-    when("#MessageVisualizer", {
-        controller: "init('Message Visualizer')",
-        activetab: "MessageVisualizer"
-    }).
-    when("#AccountsAPI", {
-        controller: "init('Accounts API')",
-        activetab: "AccountsAPI"
-
-    }).
-    when("#PeersAPI", {
-        controller: "init('Peers API')",
-        activetab: "PeersAPI"
-    }).
-    when("#SuggestionSearch", {
-        controller: "init('Suggestion Search')",
-        activetab: "SuggestionSearch"
-
-    }).
-    when("#TweetSearch", {
-        controller: "init('Tweet search')",
-        activetab: "TweetSearch"
-    }). 
-    when("#All", {
-        controller: "init('All')",
-        activetab: "All"
-    }).
-    when("#UsersAPI", {
-        controller: "init('Users API')",
-        activetab: "UsersAPI"
-    });
 });
