78280de9087b3bbe54d8edebe68b91d16ea908b7
Kaisar Arkhan <ykno@protonmail.com>
2017-01-13 08:45:01 +0700

    Travis: Deploy documentations on gh-pages for deployment and master
    Closes https://github.com/loklak/loklak_server/issues/964

--- .utility/push-docs-to-gh-pages.sh
+++ .utility/push-docs-to-gh-pages.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" = "loklak/loklak_server" ] && [ "$TRAVIS_JDK_VERSION" = "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+echo -e "Creating javadoc...\n"
 
-  echo -e "Creating javadoc...\n"
+ant javadoc
 
-  ant javadoc
+echo -e "Publishing javadoc...\n"
 
-  echo -e "Publishing javadoc...\n"
+cp -R html/javadoc $HOME/javadoc-latest
 
-  cp -R html/javadoc $HOME/javadoc-latest
+echo -e "Installing requirements...\n"
 
-  echo -e "Installing requirements...\n"
+pip3 install -r docs/requirements.txt
 
-  pip3 install -r docs/requirements.txt
+echo -e "Generating static HTML pages for documentation...\n"
 
-  echo -e "Generating static HTML pages for documentation...\n"
+cd docs
+make html
 
-  cd docs
-  make html
+echo -e "Publishing documentation...\n"
 
-  echo -e "Publishing documentation...\n"
+cp -Rf _build $HOME/docs
 
-  cp -Rf _build $HOME/docs
-  
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/loklak/loklak_server gh-pages > /dev/null
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/loklak/loklak_server gh-pages > /dev/null
 
-  cd gh-pages
-  git rm -rf ./*
-  cp -Rf $HOME/docs/* .
-  cp -Rf $HOME/javadoc-latest ./javadoc
-  git add -f .
-  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null 2>&1
+cd gh-pages
+git rm -rf ./*
+cp -Rf $HOME/docs/* .
+cp -Rf $HOME/javadoc-latest ./javadoc
+git add -f .
+git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null 2>&1
 
-  if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "Published Javadoc to gh-pages.\n"
     exit 0
-  else
+else
     echo -e "Publishing failed. Maybe the access-token was invalid or had insufficient permissions.\n"
     exit 1
-  fi
-  
 fi
-
