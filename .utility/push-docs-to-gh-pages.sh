90e51d7bd5f582f0aeea764a1a4543deae3583e9
Kaisar Arkhan (Yuki) <ykno@protonmail.com>
2017-01-13 18:22:49 +0700

    Travis: Add CNAME on gh-pages
    Closes https://github.com/loklak/loklak_server/issues/976

--- .utility/push-docs-to-gh-pages.sh
+++ .utility/push-docs-to-gh-pages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo -e "Copying CNAME ...\n"
+
+cp .utility/CNAME $HOME
+
 echo -e "Creating javadoc...\n"
 
 ant javadoc
@@ -30,6 +34,7 @@ cd gh-pages
 git rm -rf ./*
 cp -Rf $HOME/docs/* .
 cp -Rf $HOME/javadoc-latest ./javadoc
+cp -f $HOME/CNAME .
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null 2>&1
