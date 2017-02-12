076744d6b5d5fed5fb258a6b81835dfc4dabc272
Kaisar Arkhan (Yuki) <yukinagato@protonmail.com>
2017-02-12 20:51:00 +0700

    Deprecation of Apache Ant (#1025)
    * Gradle: Use Maven central repository instead of lib folder
    
    Closes https://github.com/loklak/loklak_server/issues/881
    
    * Gradle: Disable doclint for Javadoc tasks
    
    Fixes https://github.com/loklak/loklak_server/issues/1024
    
    * Deprecate Apache Ant
    
    This commit fully deprecates Apache Ant and migrates existing scripts,
    etc to use Gradle over Apache Ant.
    
    The build.xml file is not removed, the tasks has been modified to use
    gradle via the wrapper to prevent system breakage for some users.
    
    Closes https://github.com/loklak/loklak_server/issues/1022

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
-sudo apt-get install -y ant openjdk-8-jdk; git clone https://github.com/loklak/loklak_server.git loklak_server
+sudo apt-get install -y openjdk-8-jdk; git clone https://github.com/loklak/loklak_server.git loklak_server
 cd loklak_server;
 sed -i.bak 's/^\(port.http=\).*/\180/'                conf/config.properties
 sed -i.bak 's/^\(port.https=\).*/\1443/'              conf/config.properties
 sed -i.bak 's/^\(upgradeInterval=\).*/\186400000000/' conf/config.properties
-ant
+./gradlew build
 bin/start.sh
