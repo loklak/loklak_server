6d4efbc49a9759022cb976457b16c3c5ab93f5e3
vibhcool <vibhcool@gmail.com>
2017-03-18 02:53:30 +0530

    Fixes #1060: Increase default Xmx value

--- bin/.preload.sh
+++ bin/.preload.sh
@@ -7,7 +7,7 @@ DFAULTCONFIG="conf/config.properties"
 CUSTOMCONFIG="data/settings/customized_config.properties"
 LOGCONFIG="conf/logs/log-to-file.properties"
 STARTUPFILE="data/startup.tmp"
-DFAULTXmx="-Xmx800m";
+DFAULTXmx="1024m";
 CUSTOMXmx=""
 
 mkdir -p data/settings
