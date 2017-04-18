ea25967ff45748b5aa0474572d742fe1415f9578
Yegor Voronyansky <vrnsky@protonmail.ch>
2017-04-18 11:32:49 +0400

    fix bug with youtube scrapper and add unit test for scraper

--- test/org/loklak/TestRunner.java
+++ test/org/loklak/TestRunner.java
@@ -3,6 +3,7 @@ package org.loklak;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.loklak.harvester.TwitterScraperTest;
+import org.loklak.harvester.YoutubeScraperTest;
 
 /*
     TestRunner for harvesters
@@ -10,6 +11,7 @@ import org.loklak.harvester.TwitterScraperTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     TwitterScraperTest.class,
+        YoutubeScraperTest.class
 })
 public class TestRunner {
 }
