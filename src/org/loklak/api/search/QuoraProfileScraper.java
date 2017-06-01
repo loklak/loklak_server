64c800adb6d3382bf4f67877fe84067ef236c4b1
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-06-01 12:32:38 +0530

    fixes #1196 - Enhanced Quora profile scraper #1199
    Fixes issue #1196 The scraper now provides more information like
    university of user, location where user works, topics he knows, number of
    followers, number of questions, number of edits, number of blogs etc.

--- src/org/loklak/api/search/QuoraProfileScraper.java
+++ src/org/loklak/api/search/QuoraProfileScraper.java
@@ -27,6 +27,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.loklak.data.DAO;
 import org.loklak.server.APIException;
 import org.loklak.server.APIHandler;
 import org.loklak.server.AbstractAPIHandler;
@@ -39,6 +42,7 @@ import org.loklak.tools.storage.JSONObjectWithDefault;
 public class QuoraProfileScraper extends AbstractAPIHandler implements APIHandler {
 
 	private static final long serialVersionUID = -3398701925784347310L;
+	private static final	 String QUORA_URL_BASE = "https://www.quora.com";
 
 	@Override
 	public String getAPIPath() {
@@ -67,15 +71,26 @@ public class QuoraProfileScraper extends AbstractAPIHandler implements APIHandle
 		JSONObject quoraProfile = new JSONObject();
 
 		Document userHTML = null;
-		String url = "https://www.quora.com/profile/" + profile;
+		String url = QUORA_URL_BASE + "/profile/" + profile;
 
 		try {
-			userHTML = Jsoup.connect(url).get();
+			userHTML = Jsoup.connect(url)
+							.header("Accept-Encoding", "gzip, deflate")
+							.userAgent("Mozilla")
+							.maxBodySize(0)
+							.timeout(600000)
+							.get();
 		} catch (IOException e) {
-			e.printStackTrace();
+			DAO.severe(e);
+			quoraProfile.put("Error", "Unable to connect to https://www.quora.com");
+			JSONArray jsonArray = new JSONArray();
+			jsonArray.put(quoraProfile);
+			SusiThought json = new SusiThought();
+			json.setData(jsonArray);
+			return json;
 		}
 
-		String bio = userHTML.getElementsByClass("qtext_para").text();
+		String bio = userHTML.getElementsByAttributeValueContaining("class", "ProfileDescription").text();
 		quoraProfile.put("bio", bio);
 
 		String profileImage = userHTML.getElementsByAttributeValueContaining("class", "profile_photo_img").attr("src");
@@ -87,8 +102,38 @@ public class QuoraProfileScraper extends AbstractAPIHandler implements APIHandle
 		String rssFeedLink = url + "/rss";
 		quoraProfile.put("rss_feed_link", rssFeedLink);
 
+		Elements userBasicInfo = userHTML.getElementsByAttributeValueContaining("class", "UserCredential IdentityCredential");
+		for (Element info: userBasicInfo) {
+			String infoText = info.text();
+			if (infoText.startsWith("Studi")) {
+				quoraProfile.put(infoText.split(" ")[0].toLowerCase().trim() + "_at", infoText);
+			} else if (infoText.startsWith("Lives")) {
+				quoraProfile.put("lives_in", infoText);
+			} else {
+				quoraProfile.put("works_at", infoText);
+			}
+		}
+
+		Elements knowsAbout = userHTML.getElementsByAttributeValueContaining("class",  "TopicNameSpan TopicName");
+		JSONArray topics = new JSONArray();
+		for (Element topic: knowsAbout) {
+			topics.put(topic.text());
+		}
+		quoraProfile.put("knows_about", topics);
+
+		JSONObject feeds = new JSONObject();
+		Elements counts = userHTML.getElementsByAttributeValueContaining("class", "list_count");
+		for (Element count: counts) {
+			String topic = count.parent().text();
+			topic = topic.substring(0, topic.indexOf(count.text())).trim();
+			feeds.put(topic.toLowerCase() + "_url", QUORA_URL_BASE + count.parent().attr("href"));
+			feeds.put(topic.toLowerCase(), count.text());
+		}
+		quoraProfile.put("feeds", feeds);
+
 		JSONArray jsonArray = new JSONArray();
 		jsonArray.put(quoraProfile);
+
 
 		SusiThought json = new SusiThought();
 		json.setData(jsonArray);
