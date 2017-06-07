345480af005978f76be58fe3ac762d3a3639c3a5
Vibhor Verma <vibhcool@gmail.com>
2017-06-07 06:41:29 +0000

    Fixes #1219: Add UTC time in TimeAndDateService (#1220)

--- src/org/loklak/api/search/TimeAndDateService.java
+++ src/org/loklak/api/search/TimeAndDateService.java
@@ -18,12 +18,15 @@
 
 package org.loklak.api.search;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.TimeZone;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.loklak.data.DAO;
 import org.loklak.server.APIException;
 import org.loklak.server.APIHandler;
 import org.loklak.server.AbstractAPIHandler;
@@ -35,41 +38,92 @@ import org.loklak.tools.storage.JSONObjectWithDefault;
 
 public class TimeAndDateService extends AbstractAPIHandler implements APIHandler {
 
-	private static final long serialVersionUID = 6808423132726076271L;
+    private static final long serialVersionUID = 6808423132726076271L;
 
-	@Override
-	public String getAPIPath() {
-		return "/api/timeanddate.json";
-	}
+    @Override
+    public String getAPIPath() {
+        return "/api/timeanddate.json";
+    }
 
-	@Override
-	public BaseUserRole getMinimalBaseUserRole() {
-		return BaseUserRole.ANONYMOUS;
-	}
+    @Override
+    public BaseUserRole getMinimalBaseUserRole() {
+        return BaseUserRole.ANONYMOUS;
+    }
 
-	@Override
-	public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
-		return null;
-	}
+    @Override
+    public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
+        return null;
+    }
 
-	public JSONObject serviceImpl(Query call, HttpServletResponse response, Authorization rights, JSONObjectWithDefault permissions)
-			throws APIException {
-		return timeAndDate();
-	}
+    public JSONObject serviceImpl(
+            Query call,
+            HttpServletResponse response,
+            Authorization rights,
+            JSONObjectWithDefault permissions
+    ) throws APIException {
+        return timeAndDate();
+    }
 
-	public static SusiThought timeAndDate() {
-		
-		JSONObject timeAndDate = new JSONObject();
-		
-		Date time_and_date = new Date();
-		timeAndDate.put("time_and_date", time_and_date.toString());
-		
-		
-		JSONArray jsonArray = new JSONArray();
-		jsonArray.put(timeAndDate);
-		
-		SusiThought result = new SusiThought();
-		result.setData(jsonArray);
-		return result;
-	}
+    public static SusiThought timeAndDate() {
+
+        JSONObject timeAndDate = new JSONObject();
+
+        Date time_and_date = new Date();
+        String time_and_date_UTC = timeToUTC(time_and_date).toString().replaceAll(
+                time_and_date.toString().substring(20,23),
+                "UTC"
+        );
+
+        timeAndDate.put("local_time_and_date", time_and_date.toString());
+        timeAndDate.put("UTC_time_and_date", time_and_date_UTC);
+        timeAndDate.put("time_diff", timeDiff(time_and_date));
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(timeAndDate);
+
+        SusiThought result = new SusiThought();
+        result.setData(jsonArray);
+        return result;
+    }
+
+    /*
+     * Convert localzone time to UTC zone time
+    */
+    private static Date timeToUTC(Date date) {
+
+        Date dateUtc;
+        SimpleDateFormat dateFormatGmt = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
+        dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"));
+        
+        //Local time zone   
+        SimpleDateFormat dateFormatLocal = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
+
+        try {
+           //Time in GMT
+           dateUtc = dateFormatLocal.parse(dateFormatGmt.format(date));
+        } catch (ParseException e) {
+            DAO.severe(e);
+            dateUtc = new Date();
+        }
+        return dateUtc;
+    }
+
+    /*
+     * Return difference between the local time and UTC time in hours
+    */
+    private static String timeDiff(Date timeLocal) {
+    
+        long diff = timeLocal.getTime() - timeToUTC(timeLocal).getTime();
+        long diffMin = diff / (60 * 1000) % 60;
+        long diffHrs = diff / (60 * 60 * 1000) % 24;
+        String min = String.format("%02d", diffMin).replace("-", "");
+        String hrs = String.format("%02d", diffHrs);
+
+        String diffTime = hrs + ":" + min;//.replace("-","");
+        if (diffHrs > 0) {
+            diffTime = "+" + diffTime;
+        }
+        return diffTime;
+    }
+
 }
