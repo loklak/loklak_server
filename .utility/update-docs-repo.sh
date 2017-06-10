32c300774e4532935178510d141c6f39bf481593
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-06-10 13:01:03 +0530

    Fixes #1226 Build error currently showing (#1228)
    * Fixes 1215 Fix relative link
    
    * Update git to work with subtree
    
    * Adding echo statements

--- .utility/update-docs-repo.sh
+++ .utility/update-docs-repo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LOC=$(pwd)
+
 cp README.rst $HOME/.
 
 git config --global user.email "travis@travis-ci.org"
@@ -13,8 +15,12 @@ git clone --quiet --branch=master git@github.com:loklak/dev.loklak.org.git lokla
 
 cd loklak_docs
 
-if ! git subtree pull --prefix=raw/server ../loklak_server documentation --squash -m "Update server subtree" ; then
-  git subtree add --prefix=raw/server ../loklak_server documentation --squash -m "Update server subtree"
+echo "Pulling subtree..."
+if git subtree pull --prefix=raw/server $LOC documentation --squash -m "Update server subtree" ; then
+  echo "Pulled successfully."
+else
+  echo "Failed to pull subtree ... Adding subtree first"
+  git subtree add --prefix=raw/server $LOC documentation --squash -m "Update server subtree"
 fi
 
 cp -f $HOME/README.rst .
