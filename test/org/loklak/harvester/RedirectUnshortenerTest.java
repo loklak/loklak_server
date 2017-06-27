d0e4502c2e797d5bb2df3a70ca71ced65aa29bab
Pratyush Singh <singh.pratyush96@gmail.com>
2017-06-27 17:48:17 +0530

    Fix #1271: Correct redirect link in test (#1272)

--- test/org/loklak/harvester/RedirectUnshortenerTest.java
+++ test/org/loklak/harvester/RedirectUnshortenerTest.java
@@ -19,7 +19,7 @@ public class RedirectUnshortenerTest {
         shortlinkMap.put("http://goo.gl/R9CVuz", "http://www.savingsgator.com/coupon/ebay-new-authentic-gucci-crystal-gg-canvasleather-joy-boston-bag-handbag/");
         shortlinkMap.put("http://tinyurl.com/pcp7fu4", "http://www.giladiskon.com/deals/blitzmegaplex.com/Blitzmegaplex-Special-Member-Card-Promo-Diskon-50");
         shortlinkMap.put("http://t.co/E3w7s2qdBT", "https://www.mostviralfeed.com/what-lady-gaga-actually-looks-like");
-        shortlinkMap.put("http://bit.ly/1h9gTTT", "http://www.ebay.com/sch/i.html?_from=R40&_nkw=Pontiac+Catalina+1963&_in_kw=1&_ex_kw=&_sacat=0&_okw=&_oexkw=&_adv=1&_udlo=&_udhi=&LH_BIN=1&LH_PayPal=1&LH_Time=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=200&_fpos=&_fsct=&LH_SALE_CURRENCY=0&_fss=1&_saslop=1&_sasl=&_fsradio=LH_SellerWithStore%3D1&_sop=12&_dmd=1&_ipg=50&rmvSB=true");
+        shortlinkMap.put("http://bit.ly/1h9gTTT", "https://www.ebay.com/sch/i.html?_from=R40&_nkw=Pontiac+Catalina+1963&_in_kw=1&_ex_kw=&_sacat=0&_okw=&_oexkw=&_adv=1&_udlo=&_udhi=&LH_BIN=1&LH_PayPal=1&LH_Time=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=200&_fpos=&_fsct=&LH_SALE_CURRENCY=0&_fss=1&_saslop=1&_sasl=&_fsradio=LH_SellerWithStore%3D1&_sop=12&_dmd=1&_ipg=50&rmvSB=true");
         shortlinkMap.put("http://ow.ly/JtOPA", "http://yacy.net/en/index.html");
         shortlinkMap.put("http://bit.do/ZwrT", "http://yacy.net/en/index.html");
         shortlinkMap.put("http://fb.me/4lcXZsyyO", "https://www.facebook.com/permalink.php?story_fbid=1550262581900846&id=1381813572079082");
