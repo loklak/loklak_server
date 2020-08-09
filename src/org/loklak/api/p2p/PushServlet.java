dc993af92884138a7bfda5faedf41ad35ee4ada9
Michael Peter Christen <mc@yacy.net>
2020-08-09 12:16:48 +0200

    fixed exception in exception log

--- src/org/loklak/api/p2p/PushServlet.java
+++ src/org/loklak/api/p2p/PushServlet.java
@@ -110,7 +110,7 @@ public class PushServlet extends HttpServlet {
                 transmittedToAtLeastOnePeer = true;
                 break pushattempts;
             } catch (IOException | JSONException | SignatureException | InvalidKeyException e) {
-                DAO.log("FAILED to push " + timeline.size() + " messages to backend " + hoststub + " - " + e.getMessage() + " - " + e.getCause().getMessage());
+                DAO.log("FAILED to push " + timeline.size() + " messages to backend " + hoststub + " - " + e.getMessage() + (e.getCause() == null ? "" : " - " + e.getCause().getMessage()));
                 DAO.severe(e);
                 try {Thread.sleep(1000);} catch (InterruptedException e1) {} // sleep to prevent that same hosts are requested too fast causing a "frequency too high" exception
             } finally {
